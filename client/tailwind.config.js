139cd0724ad68824962bf5c8ff22fe2c295edbe8
Dennis Soukhikh <dennis.soukhikh@outlook.ie>
2021-07-03 13:21:44 +0100

    add tailwind css

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.tsx', './public/index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
