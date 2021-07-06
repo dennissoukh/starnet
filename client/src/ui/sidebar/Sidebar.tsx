b9d1c4b23c5f31af5d513e1bef837c31d794472c
Dennis Soukhikh <dennis.soukhikh@outlook.ie>
2021-07-06 13:59:36 +0100

    sidebar design fix

--- client/src/ui/sidebar/Sidebar.tsx
+++ client/src/ui/sidebar/Sidebar.tsx
@@ -8,32 +8,32 @@ const Sidebar: React.FC = () => {
       className="absolute top-0 left-0 flex items-center justify-center"
     >
       <div className="text-primary-300">
-        <div>
+        <div className="bg-primary-700 p-3 rounded-lg">
           <a href="/">
             <HiHome size={25}/>
           </a>
         </div>
-        <div className="mt-10">
+        <div className="mt-4 p-3">
           <a href="/">
             <HiSparkles size={25}/>
           </a>
         </div>
-        <div className="mt-10">
+        <div className="mt-4 p-3">
           <a href="/">
             <HiGlobe size={25}/>
           </a>
         </div>
-        <div className="mt-10">
+        <div className="mt-4 p-3">
           <a href="/">
             <HiSun size={25}/>
           </a>
         </div>
-        <div className="mt-10">
+        <div className="mt-4 p-3">
           <a href="/">
             <HiMoon size={25}/>
           </a>
         </div>
-        <div className="mt-10">
+        <div className="mt-4 p-3">
           <a href="/">
             <HiCog size={25}/>
           </a>
