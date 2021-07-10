90db939bacabf12fc210acccf8357a66d18dd9b0
Dennis Soukhikh <dennis.soukhikh@outlook.ie>
2021-07-10 15:05:44 +0100

    add modal component

--- client/src/modules/main/Overview.tsx
+++ client/src/modules/main/Overview.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { VscAdd, VscSearch } from 'react-icons/vsc';
 import { useApplicationStore } from '../../global-stores/useApplicationStore';
 import { convertCoordinatesDMS } from '../../utils/converters';
+import { Modal } from '../../shared-components/Modal';
 
 export const Overview: React.FC = () => {
+  const [modalActive, setModalActive] = useState(false);
   const geolocation = useApplicationStore(state => state.geolocation);
 
   return (
@@ -21,11 +23,18 @@ export const Overview: React.FC = () => {
         <div className="px-10 py-6 border-b border-solid border-primary-800">
           <div className="flex justify-between items-center text-primary-200">
             <span className="font-light text-tiny uppercase">Location</span>
-            <div className="cursor-pointer">
+            <div className="cursor-pointer" onClick={() => setModalActive(!modalActive)}>
               <VscAdd size={13}/>
             </div>
           </div>
           <p className="text-xl tracking-wide">Earth, {convertCoordinatesDMS(geolocation.latitude)}, {convertCoordinatesDMS(geolocation.longitude)}</p>
+          <Modal
+            callback={setModalActive}
+            isVisible={modalActive}
+            title="Location"
+          >
+            <span>ChIldren</span>
+          </Modal>
         </div>
         <div className="border-b border-solid border-primary-800 flex">
           <div className="w-1/2 px-10 py-6 border-r border-solid border-primary-800">
---
+++ client/src/shared-components/Modal.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { VscClose } from 'react-icons/vsc';
+import { motion, AnimatePresence } from 'framer-motion';
+
+interface Props {
+  callback: Function,
+  isVisible: boolean,
+  title?: string
+};
+
+const duration = 0.085;
+
+const variants = {
+  initial: {
+    opacity: 0,
+  },
+  enter: {
+    opacity: 1,
+    transition: {
+      duration: duration,
+      delay: duration,
+      when: "beforeChildren",
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: { duration: duration },
+  },
+};
+
+export const Modal: React.FC<Props> = ({ callback, isVisible, children, title }) => {
+  return (
+    <AnimatePresence>
+      {isVisible &&
+        <motion.div
+          key="modal"
+          variants={variants}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+        >
+          <div
+            style={{ backgroundColor: 'rgba(0, 0, 0, .3)'}}
+            className="fixed w-screen h-screen top-0 left-1/2 transform -translate-x-1/2 z-10"
+            onClick={() => callback(false)}
+          ></div>
+          <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20">
+            <div
+              className="bg-primary-100 rounded-xl shadow-lg m-3 md:m-6 overflow-hidden"
+              style={{ maxWidth: '900px', width: '95vw',  }}
+            >
+              <div className="overflow-y-auto px-5 py-7 md:p-7 text-primary-900" style={{ maxHeight: '570px' }}>
+                <div className="w-full flex justify-between items-center">
+                  <div>
+                    <span className="text-tiny uppercase">{title}</span>
+                  </div>
+                  <div>
+                    <VscClose size={25} className="cursor-pointer" onClick={() => callback()}/>
+                  </div>
+                </div>
+                <div>
+                  {children}
+                </div>
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      }
+    </AnimatePresence>
+  )
+}
+
