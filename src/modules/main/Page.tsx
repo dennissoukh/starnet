70972be02a134a5d0a2a19d97a50c2e29de22a2a
Dennis Soukhikh <dennis.soukhikh@outlook.ie>
2021-07-28 17:50:23 +0100

    disable geolocation for testing

--- src/modules/main/Page.tsx
+++ src/modules/main/Page.tsx
@@ -6,9 +6,9 @@ import { Overview } from './Overview';
 export const MainPage: React.FC = () => {
   return (
     <div className="flex flex-col lg:flex-row">
-      {/* <div className="w-2/3 lg:min-h-screen flex items-center justify-center p-5" style={{ minWidth: '950px' }}>
+      <div className="w-2/3 lg:min-h-screen flex items-center justify-center p-5" style={{ minWidth: '950px' }}>
         <Chart/>
-      </div> */}
+      </div>
       <div className="border-l border-solid border-primary-800 mt-32 lg:mt-0 lg:min-w-620 w-full lg:w-1/3">
         <div className="flex flex-col min-h-full">
           <Overview/>
